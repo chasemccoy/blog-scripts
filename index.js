2411457c171675bfdc42500b7ef6462b0f4a938b
Chase McCoy <chasem000@gmail.com>
2019-03-04 14:26:11 -0600

    add date grouping

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { createError, json, text } = require("micro");
-
+const { format, isSameDay } = require('date-fns');
 const base = require('airtable').base('appUMyBbge0Itf7aP');
+
+const DATE_FORMAT = 'MMMM Do';
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,8 +12,24 @@ module.exports = async (req, res) => {
   }).firstPage()
 
   let response = '';
+  let currentDate = null;
 
   table.forEach(record => {
+    const date = new Date(record.get('Date Added'));
+
+    if (currentDate === null) {
+      response += `# ${format(date, DATE_FORMAT)} \n\n`
+    }
+    else {
+      const sameDay = isSameDay(date, currentDate)
+
+      if (!sameDay) {
+        response += `# ${format(date, DATE_FORMAT)} \n\n`
+      }
+    }
+
+    currentDate = date;
+
     response += `**${record.get('Title')}**\n`
     response += record.get('URL')
     response += '\n\n'
