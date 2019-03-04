9c87a3d7627fcb096907e95ec5175075b1e46329
Chase McCoy <chasem000@gmail.com>
2019-03-04 13:50:43 -0600

    hook up Airtable

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { createError, json, text } = require("micro");
 
+const base = require('airtable').base('appUMyBbge0Itf7aP');
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  return 'Hi Chase!';
+
+  const table = await base('Table 1').select({view: 'Grid view'}).firstPage()
+
+  table.forEach(record => (
+    console.log(record.get('Title'))
+  ))
+
+  return table.map(record => record.get('Title'))
+
+  return 'Failed.';
 };
